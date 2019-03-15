e2e10a9a1eabe0b7b4156fae2d5d49f26625e2fb
WangHanHong <35711983+WangHanHong@users.noreply.github.com>
2019-03-15 17:53:38 +0800

    购买之前登录的模态框制作

--- src/main/java/com/wuit/dx/controller/ForeRESTController.java
+++ src/main/java/com/wuit/dx/controller/ForeRESTController.java
@@ -1,15 +1,17 @@
 package com.wuit.dx.controller;
 
 import com.wuit.dx.dao.ProductDAO;
+import com.wuit.dx.entity.LocahAuth;
 import com.wuit.dx.entity.Product;
 import com.wuit.dx.entity.ProductCategory;
+import com.wuit.dx.service.LocalAuthService;
 import com.wuit.dx.service.ProductCategoryService;
 import com.wuit.dx.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.wuit.dx.util.Result;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,6 +26,9 @@ public class ForeRESTController {
 
     @Resource
     private ProductService productService;
+
+    @Resource
+    private LocalAuthService localAuthService;
 
     @GetMapping("/forehome")
     public Object home(){
@@ -40,6 +45,33 @@ public class ForeRESTController {
     public Object product(@PathVariable("pid") int pid) {
         Product p= productService.getProductById(pid);
         return  p;
+    }
+
+    /**
+     * 是否登录验证
+     * @param session
+     * @return
+     */
+    @GetMapping("forecheckLogin")
+    public Object checkLogin( HttpSession session) {
+        LocahAuth user =(LocahAuth)  session.getAttribute("user");
+        if(null!=user) {
+            return Result.success();
+        }
+        return Result.fail("未登录");
+    }
+
+    @PostMapping("forelogin")
+    public Object login(@RequestBody LocahAuth user, HttpSession session){
+        LocahAuth auth=  localAuthService.loginAuth(user);
+        if(null==auth){
+            String message ="账号密码错误";
+            return Result.fail(message);
+        }
+        else{
+            session.setAttribute("user", auth);
+            return Result.success();
+        }
     }
 
 
